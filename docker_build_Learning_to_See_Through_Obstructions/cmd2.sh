1d92c42fe16f107eb245dedd4363f4bddd997083
Ruinan Zhao <z_ruinan@hotmail.com>
2022-07-19 21:59:11 +0900

    change to python3.6

--- docker_build_Learning_to_See_Through_Obstructions/cmd2.sh
+++ docker_build_Learning_to_See_Through_Obstructions/cmd2.sh
@@ -5,6 +5,7 @@ git clone https://github.com/philferriere/tfoptflow.git ObstructionRemoval/tfopt
 cp -rf ObstructionRemoval/temp_tfoptflow/tfoptflow/tfoptflow/model_pwcnet.py ObstructionRemoval/tfoptflow/tfoptflow/model_pwcnet.py 
 cp -rf ObstructionRemoval/temp_tfoptflow/tfoptflow/tfoptflow/model_base.py ObstructionRemoval/tfoptflow/tfoptflow/model_base.py 
 mkdir ObstructionRemoval/code_and_ckpt 
+mkdir ObstructionRemoval/output
 cd ObstructionRemoval/code_and_ckpt
 wget https://www.cmlab.csie.ntu.edu.tw/~yulunliu/ObstructionRemoval_/code_and_ckpt.zip
 unzip code_and_ckpt.zip 
